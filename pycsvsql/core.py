41db4de9358bc8a34d0035096b1cab2770dd2dd0
gzxultra <mygladfinger@gmail.com>
2018-05-02 15:57:31 +0800

    Implement data convertor

--- pycsvsql/core.py
+++ pycsvsql/core.py
@@ -1,9 +1,51 @@
+# coding: utf-8
+import datetime
+
 import click
+from peewee import CharField, DateTimeField, Model, SqliteDatabase
+from pycsvsql.helpers import next_n_lines
+
+
+db = SqliteDatabase('pycsvsql.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+    created_time = DateTimeField(default=datetime.datetime.now)
+    update_time = DateTimeField(default=datetime.datetime.now)
+
+
+def init_database(filename):
+    f = open(filename, 'r')
+    headers = f.readline()
+    fields = []
+    for col_name in headers.strip().split(','):
+        field = CharField(null=False, default='')
+        fields.append(field)
+        BaseModel._meta.add_field(col_name, field)
+        BaseModel._meta.table_name = filename
+
+    BaseModel.create_table()
+
+    with db.atomic():
+        while True:
+            lines = next_n_lines(f, 100)
+            if not lines:
+                break
+            data_source = [i.split(',') for i in lines]
+            BaseModel.insert_many(data_source, fields=fields).execute()
+
+    f.close()
+    return BaseModel
+
 
 @click.command()
 @click.option('--file', default='sample.csv', prompt='name of your csv file')
 def pycsvsql(file):
-    print(file)
+    model_clazz = init_database(file)
+    return model_clazz
 
 
 if __name__ == '__main__':
--- pycsvsql/helpers.py
+++ pycsvsql/helpers.py
@@ -1,21 +1,6 @@
-import datetime
-import pandas as pd
-from peewee import Model, DateTimeField, CharField, SqliteDatabase
-
-db = SqliteDatabase('pycsvsql.db')
-
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-    created_time= DateTimeField(default=datetime.datetime.now)
-    update_time = DateTimeField(default=datetime.datetime.now)
+# coding: utf-8
+from itertools import islice
 
 
-def read_csv(filename):
-    df = pd.read_csv(filename)
-    for col_name in df.columns:
-        field = CharField(null=False, default='')
-        BaseModel._meta.add_field(col_name, field)
-        BaseModel._meta.table_name = filename
-    return BaseModel
+def next_n_lines(file, n_lines):
+    return [x.strip() for x in islice(file, n_lines)]
